ad9119be29ad50185d7cf51a87699be8a7cf7306
Richard Lin <elpato25@gmail.com>
2025-04-14 00:48:05 -0700

    bits and pieces

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SubLayout
 KiCad plugin to merge (or create) a sub-pcb-layout into (or from) a top-level board.
+
+
+Inspired by [HierarchicalPcb](https://github.com/gauravmm/HierarchicalPcb) and [SaveRestoreLayout](https://github.com/MitjaNemec/SaveRestoreLayout)
--- sublayout.py
+++ sublayout.py
@@ -3,6 +3,26 @@ import os
 import wx
 
 
+class SubLayoutFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, title="SubLayout", size=(300, 200))
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.text_ctrl = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        sizer.Add(self.text_ctrl, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.button = wx.Button(panel, label="Close")
+        sizer.Add(self.button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        panel.SetSizer(sizer)
+
+        self.Bind(wx.EVT_BUTTON, self.on_close_button_click, self.button)
+
+    def on_close_button_click(self, event):
+        self.Close()
+
+
 class SubLayout(pcbnew.ActionPlugin):
     def defaults(self):
         self.name = "Sublayout"
@@ -12,5 +32,20 @@ class SubLayout(pcbnew.ActionPlugin):
         # self.icon_file_name = os.path.join(os.path.dirname(__file__), 'simple_plugin.png') # Optional, defaults to ""
 
     def Run(self):
-        # The entry function of the plugin that is executed on user action
-        wx.MessageBox("Hello World")
+        editor = wx.FindWindowByName("PcbFrame")
+        board = pcbnew.GetBoard()  # type: pcbnew.BOARD
+
+        msg = ""
+        for fp in board.GetFootprints():  # type: pcbnew.FOOTPRINT
+            fp.IsSelected()
+            fp_path = fp.GetPath()  # type: pcbnew.KIID_PATH
+            fp.GetFieldsText()
+
+            fp.GetSheetfile()  # name of containing hierarchical block file
+            fp.GetSheetname()  # name of containing hierarchical block instance
+            msg += f"{fp.GetReference()} {fp.GetFPIDAsString()} {fp_path.AsString()}  {fp.GetSheetfile()}  {fp.GetSheetname()}\n"
+
+        wx.MessageBox(f"Hello {msg}")
+        self.frame = SubLayoutFrame(editor)
+        self.frame.Show()
+
