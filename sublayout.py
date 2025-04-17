18cc1e85450b5799f175cb8b14f832eed505a2e8
Richard Lin <elpato25@gmail.com>
2025-04-17 00:40:21 -0700

    cleaning

--- sublayout.py
+++ sublayout.py
@@ -5,48 +5,7 @@ import os
 import wx
 
 
-class BoardUtils():
-    @classmethod
-    def highlight_footprint(cls, footprint: pcbnew.FOOTPRINT, bright: bool = True) -> None:
-        """Highlight a footprint on the board."""
-        if bright:
-            footprint.SetBrightened()
-            for pad in footprint.Pads():  # type: pcbnew.PAD
-                pad.SetBrightened()
-        else:
-            footprint.ClearBrightened()
-            for pad in footprint.Pads():  # type: pcbnew.PAD
-                pad.ClearBrightened()
-
-    @classmethod
-    def footprint_path(cls, footprint: pcbnew.FOOTPRINT) -> Tuple[str, ...]:
-        fp_path = footprint.GetPath()  # type: pcbnew.KIID_PATH
-        return tuple(cast(str, fp_path.AsString()).strip('/').split('/'))
-
-    @classmethod
-    def footprint_path_startswith(cls, footprint: pcbnew.FOOTPRINT, path_prefix: Tuple[str, ...]) -> bool:
-        """Returns true if the footprint path starts with the given prefix (is part of the path_prefix hierarchy)"""
-        fp_path = cls.footprint_path(footprint)
-        return fp_path[:len(path_prefix)] == path_prefix
-
-    @classmethod
-    def calculate_path_sheetfile_names(cls, footprints: List[pcbnew.FOOTPRINT]) -> Dict[Tuple[str, ...], Tuple[str, str]]:
-        """Iterates through footprints in the board to try to determine the sheetfile and sheetname
-        associated with a path."""
-        path_sheetfile_names = {}
-        for fp in footprints:
-            fp_path_comps = cls.footprint_path(fp)
-            if len(fp_path_comps) < 2:  # ignore root components
-                continue
-            fp_path_comps = fp_path_comps[:-1]  # remove the last component (leaf footprint)
-            sheetfile_name = (cast(str, fp.GetSheetfile()), cast(str, fp.GetSheetname()))
-            if not sheetfile_name[0] or not sheetfile_name[1]:
-                continue
-            if fp_path_comps in path_sheetfile_names:
-                assert path_sheetfile_names[fp_path_comps] == sheetfile_name
-            else:
-                path_sheetfile_names[fp_path_comps] = sheetfile_name
-        return path_sheetfile_names
+from .board_utils import BoardUtils
 
 
 class SubLayoutFrame(wx.Frame):
--- test_save.py
+++ test_save.py
@@ -2,8 +2,12 @@ import os
 import unittest
 import pcbnew
 
+from board_utils import BoardUtils
+
 
 class SaveTestCase(unittest.TestCase):
   def test_save(self):
-    board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__),'tests', 'TestBlinkyComplete.kicad_pcb'))
-    print(board)
+    board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'TestBlinkyComplete.kicad_pcb'))
+    sublayout_board = BoardUtils.duplicate_board(board)
+    sublayout_board.Save('test_output_saved.kicad_pcb')
+    print(sublayout_board)
