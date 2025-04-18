ec1c807909a6b3e53f9506b3ef7c6c99c70b38e4
Richard Lin <elpato25@gmail.com>
2025-04-18 00:29:10 -0700

    more save filtering

--- save_sublayout.py
+++ save_sublayout.py
@@ -7,18 +7,25 @@ from board_utils import BoardUtils
 
 class SaveSublayout():
     """Function and utilities to save a sub-layout given a hierarchy path prefix.
-    Does not affect the original board obect."""
+    Does not affect the original board object."""
     def __init__(self, board: pcbnew.BOARD, path_prefix: Tuple[str, ...]) -> None:
         self.board = BoardUtils.duplicate_board(board)  # create working copy
         self.path_prefix = path_prefix
-        self._filter_footprints()
+        self._filter_board()
 
-    def _filter_footprints(self) -> None:
+    def _filter_board(self) -> None:
         """Filters the footprints on the board to only include those in the given hierarchy path prefix."""
         footprints = self.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
         for footprint in footprints:
             if not BoardUtils.footprint_path_startswith(footprint, self.path_prefix):
                 self.board.Delete(footprint)
+        for track in self.board.GetTracks():  # type: List[pcbnew.PCB_TRACK]
+            self.board.Delete(track)
+        for drawing in self.board.GetDrawings():  # type: List[pcbnew.DRAWINGS]
+            self.board.Delete(drawing)
+        for zone_id in range(self.board.GetAreaCount()):
+            zone = self.board.GetArea(zone_id)
+            self.board.Delete(zone)
 
     def save(self, filename: str) -> None:
         """Saves the sub-layout to a new board file and returns the new board object."""
--- test_save.py
+++ test_save.py
@@ -1,6 +1,5 @@
 import os
 import unittest
-from typing import List
 
 import pcbnew
 
@@ -12,24 +11,21 @@ class SaveTestCase(unittest.TestCase):
     def test_save(self):
         src_board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'TestBlinkyComplete.kicad_pcb'))
         save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('U2'))[:-1])
-        save.board.Save('test_mcu.kicad_pcb')
-        footprints = save.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
-        footprint_refs = {footprint.GetReference() for footprint in footprints}
+        save.board.Save('test_output_mcu.kicad_pcb')
+        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
         self.assertEqual(footprint_refs, {'U2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'J2'})
+        self.assertEqual(save.board.GetAreaCount(), 0)
 
         save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('D1'))[:-1])
-        footprints = save.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
-        footprint_refs = {footprint.GetReference() for footprint in footprints}
+        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
         self.assertEqual(footprint_refs, {'D1', 'R3'})
 
         # test that sub-hierarchy items are included
         save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('J1'))[:-1])
-        footprints = save.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
-        footprint_refs = {footprint.GetReference() for footprint in footprints}
+        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
         self.assertEqual(footprint_refs, {'J1', 'R1', 'R2'})
 
         # test that outer hierarchy items are not included
         save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('R1'))[:-1])
-        footprints = save.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
-        footprint_refs = {footprint.GetReference() for footprint in footprints}
+        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
         self.assertEqual(footprint_refs, {'R1', 'R2'})
