9d9851e6657b8a8922d99701421b43c0e0dd9963
Richard Lin <elpato25@gmail.com>
2025-04-17 23:36:43 -0700

    namer util class

--- board_utils.py
+++ board_utils.py
@@ -29,25 +29,6 @@ class BoardUtils():
         return fp_path[:len(path_prefix)] == path_prefix
 
     @classmethod
-    def calculate_path_sheetfile_names(cls, footprints: List[pcbnew.FOOTPRINT]) -> Dict[Tuple[str, ...], Tuple[str, str]]:
-        """Iterates through footprints in the board to try to determine the sheetfile and sheetname
-        associated with a path."""
-        path_sheetfile_names = {}
-        for fp in footprints:
-            fp_path_comps = cls.footprint_path(fp)
-            if len(fp_path_comps) < 2:  # ignore root components
-                continue
-            fp_path_comps = fp_path_comps[:-1]  # remove the last component (leaf footprint)
-            sheetfile_name = (cast(str, fp.GetSheetfile()), cast(str, fp.GetSheetname()))
-            if not sheetfile_name[0] or not sheetfile_name[1]:
-                continue
-            if fp_path_comps in path_sheetfile_names:
-                assert path_sheetfile_names[fp_path_comps] == sheetfile_name
-            else:
-                path_sheetfile_names[fp_path_comps] = sheetfile_name
-        return path_sheetfile_names
-
-    @classmethod
     def duplicate_board(cls, board: pcbnew.BOARD) -> pcbnew.BOARD:
         """Duplicates the board by saving and loading."""
         assert not os.path.exists('temp.kicad_pcb'), 'temp board file must not exist'
---
+++ hierarchy_namer.py
@@ -0,0 +1,48 @@
+from typing import List, Dict, Tuple, cast
+
+import pcbnew
+
+from board_utils import BoardUtils
+
+
+class HierarchyNamer:
+    """Infers meaningful names for footprints based on hierarchy sheetnames."""
+    @classmethod
+    def _build_sheetfile_names(cls, footprints: List[pcbnew.FOOTPRINT]) -> Dict[Tuple[str, ...], Tuple[str, str]]:
+        """Iterates through footprints in the board to try to determine the sheetfile and sheetname
+        associated with a path."""
+        path_sheetfile_names = {}
+        for fp in footprints:
+            fp_path_comps = BoardUtils.footprint_path(fp)
+            if len(fp_path_comps) < 2:  # ignore root components
+                continue
+            fp_path_comps = fp_path_comps[:-1]  # remove the last component (leaf footprint)
+            sheetfile_name = (cast(str, fp.GetSheetfile()), cast(str, fp.GetSheetname()))
+            if not sheetfile_name[0] or not sheetfile_name[1]:
+                continue
+            if fp_path_comps in path_sheetfile_names:
+                assert path_sheetfile_names[fp_path_comps] == sheetfile_name
+            else:
+                path_sheetfile_names[fp_path_comps] = sheetfile_name
+        return path_sheetfile_names
+
+    def __init__(self, board: pcbnew.BOARD) -> None:
+        self._sheetfile_names = self._build_sheetfile_names(board.Footprints())
+
+    def name_path(self, path: Tuple[str, ...]) -> Tuple[str, ...]:
+        """Infers a structured name for the given path"""
+        names = []
+        for i in range(1, len(path) + 1):
+            path_comps = path[:i]
+            if path_comps in self._sheetfile_names:
+                sheetfile, sheetname = self._sheetfile_names[path_comps]
+                names.append(sheetname)
+            else:
+                names.append('?')
+        return tuple(names)
+
+    def containing_name(self, footprint: pcbnew.FOOTPRINT) -> str:
+        """Returns the name of the sheet containing the footprint"""
+        fp_path = BoardUtils.footprint_path(footprint)
+        comp_names = self.name_path(fp_path[:-1])
+        return '/'.join(comp_names)
