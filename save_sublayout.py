50ccbde63deca48121ce30c78044dc3fe55f30c5
Richard Lin <elpato25@gmail.com>
2025-04-18 22:20:51 -0700

    refactor to be a bit more functional

--- save_sublayout.py
+++ save_sublayout.py
@@ -6,37 +6,50 @@ from board_utils import BoardUtils
 
 
 class SaveSublayout():
-    """Function and utilities to save a sub-layout given a hierarchy path prefix.
-    Does not affect the original board object."""
-    def __init__(self, board: pcbnew.BOARD, path_prefix: Tuple[str, ...]) -> None:
-        self.board = BoardUtils.duplicate_board(board)  # create working copy
-        self.path_prefix = path_prefix
-        self._filter_board()
+    def create_sublayout(self) -> pcbnew.BOARD:
+        """Creates a (copy) board with only the sublayout."""
+        board = BoardUtils.duplicate_board(self._board)  # create working copy
+        in_scope, out_scope = self._filter_board(board, self.path_prefix)
+        for item in out_scope:
+            board.Delete(item)
+        return board
 
-    def _filter_board(self) -> None:
-        """Filters the footprints on the board to only include those in the given hierarchy path prefix,
-        and tracks that are part of nets between the footprints in the hierarchy."""
-        footprints = self.board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
+    def __init__(self, board: pcbnew.BOARD, path_prefix: Tuple[str, ...]) -> None:
+        self._board = board
+        self.path_prefix = path_prefix
+
+    @classmethod
+    def _filter_board(cls, board: pcbnew.BOARD, path_prefix: Tuple[str, ...]) ->\
+        Tuple[List[pcbnew.BOARD_ITEM], List[pcbnew.BOARD_ITEM]]:
+        """Filters the footprints on the board, returning those that are in scope and those that are out of scope."""
+        in_scope: List[pcbnew.BOARD_ITEM] = []
+        out_scope: List[pcbnew.BOARD_ITEM] = []
         include_netcodes = set()  # nets that are part of the hierarchy
         exclude_netcodes = set()  # nets that are part of footprints not part of the hierarchy
+
+        footprints = board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
         for footprint in footprints:
-            if not BoardUtils.footprint_path_startswith(footprint, self.path_prefix):
+            if not BoardUtils.footprint_path_startswith(footprint, path_prefix):
                 for pad in footprint.Pads():  # type: pcbnew.PAD
                     exclude_netcodes.add(pad.GetNetCode())
-                self.board.Delete(footprint)
+                out_scope.append(footprint)
             else:
                 for pad in footprint.Pads():  # type: pcbnew.PAD
                     include_netcodes.add(pad.GetNetCode())
-        for track in self.board.GetTracks():  # type: List[pcbnew.PCB_TRACK]
+                in_scope.append(footprint)
+        for track in board.GetTracks():  # type: List[pcbnew.PCB_TRACK]
             include_netcodes = include_netcodes.difference(exclude_netcodes)
             if track.GetNetCode() not in include_netcodes:
-                self.board.Delete(track)
-        for drawing in self.board.GetDrawings():  # type: List[pcbnew.DRAWINGS]
-            self.board.Delete(drawing)
-        for zone_id in range(self.board.GetAreaCount()):
-            zone = self.board.GetArea(zone_id)
-            self.board.Delete(zone)
+                out_scope.append(track)
+            else:
+                in_scope.append(track)
+        for drawing in board.GetDrawings():  # type: List[pcbnew.DRAWINGS]
+            out_scope.append(drawing)
+        for zone_id in range(board.GetAreaCount()):
+            zone = board.GetArea(zone_id)  # type: pcbnew.ZONE
+            if zone.GetNetCode() not in include_netcodes:
+                out_scope.append(zone)
+            else:
+                in_scope.append(zone)
 
-    def save(self, filename: str) -> None:
-        """Saves the sub-layout to a new board file and returns the new board object."""
-        self.board.Save(filename)
+        return in_scope, out_scope
--- test_save.py
+++ test_save.py
@@ -10,22 +10,26 @@ from save_sublayout import SaveSublayout
 class SaveTestCase(unittest.TestCase):
     def test_save(self):
         src_board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'TestBlinkyComplete.kicad_pcb'))
-        save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('U2'))[:-1])
-        save.board.Save('test_output_mcu.kicad_pcb')
-        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
+        board = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('U2'))[:-1]
+                              ).create_sublayout()
+        board.Save('test_output_mcu.kicad_pcb')
+        footprint_refs = {footprint.GetReference() for footprint in board.GetFootprints()}
         self.assertEqual(footprint_refs, {'U2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'J2'})
-        self.assertEqual(save.board.GetAreaCount(), 0)
+        self.assertEqual(board.GetAreaCount(), 0)
 
-        save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('D1'))[:-1])
-        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
+        board = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('D1'))[:-1]
+                              ).create_sublayout()
+        footprint_refs = {footprint.GetReference() for footprint in board.GetFootprints()}
         self.assertEqual(footprint_refs, {'D1', 'R3'})
 
         # test that sub-hierarchy items are included
-        save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('J1'))[:-1])
-        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
+        board = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('J1'))[:-1]
+                              ).create_sublayout()
+        footprint_refs = {footprint.GetReference() for footprint in board.GetFootprints()}
         self.assertEqual(footprint_refs, {'J1', 'R1', 'R2'})
 
         # test that outer hierarchy items are not included
-        save = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('R1'))[:-1])
-        footprint_refs = {footprint.GetReference() for footprint in save.board.GetFootprints()}
+        board = SaveSublayout(src_board, BoardUtils.footprint_path(src_board.FindFootprintByReference('R1'))[:-1]
+                              ).create_sublayout()
+        footprint_refs = {footprint.GetReference() for footprint in board.GetFootprints()}
         self.assertEqual(footprint_refs, {'R1', 'R2'})
