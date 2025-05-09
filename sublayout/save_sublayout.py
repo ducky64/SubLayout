e65017123d246034e24f9a034230aee8382f430b
Richard Lin <elpato25@gmail.com>
2025-05-09 01:17:24 -0700

    wip groupaware filter op

--- sublayout/save_sublayout.py
+++ sublayout/save_sublayout.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Dict, Set, Optional, NamedTuple
+from typing import Tuple, List, Dict, Set, Optional, NamedTuple, Union
 
 import pcbnew
 
@@ -7,11 +7,8 @@ from .hierarchy_namer import HierarchyNamer
 
 
 class FilterResult(NamedTuple):
-    footprints: List[pcbnew.FOOTPRINT]
-    tracks: List[pcbnew.PCB_TRACK]  # includes vias
-    zones: List[pcbnew.ZONE]
-    groups: Set[GroupWrapper]  # excluding root group, if any
-    root_group: Optional[GroupWrapper]
+    elts: List[Union[pcbnew.FOOTPRINT, pcbnew.PCB_TRACK, pcbnew.ZONE]]
+    groups: Set[GroupWrapper]  # groups that are wholly part of the hierarchy
 
 
 class SaveSublayout():
@@ -19,6 +16,16 @@ class SaveSublayout():
         """Creates a (copy) board with only the sublayout."""
         board = pcbnew.CreateEmptyBoard()  # type: pcbnew.BOARD
         result = self._filter_board()
+
+        # TODO copy groups to new board, ignoring top-level group
+        # algo: gather items by group, including ungrouped items
+        # for all groups, check for conflicts (groups containing other footprints, even recursively)
+        #   move items to ungrouped
+        # for remaining groups, remove sub-groups
+        # replicate groups, recursively, maintaining group structure
+        #   exception: if there is only one remaining group, it is the top-level board
+
+
         nets_by_netcode: Dict[int, pcbnew.NETINFO_ITEM] = self._board.GetNetsByNetcode()
         for netcode, net in nets_by_netcode.items():
             board.Add(net)
@@ -33,46 +40,50 @@ class SaveSublayout():
 
     def _filter_board(self) -> FilterResult:
         """Filters the footprints on the board, returning those that are in scope."""
-        result = FilterResult([], [], [], set(), None)
         include_netcodes: Set[int] = set()  # nets that are part of the hierarchy
         exclude_netcodes: Set[int] = set()  # nets that are part of footprints not part of the hierarchy
-        has_ungrouped_footprints = False
-        exclude_groups: Set[GroupWrapper] = set()  # we don't care about None here
+
+        # footprints and tracks / zones of internal netlists, keyed by group
+        elts_by_group: Dict[GroupWrapper, List[Union[pcbnew.FOOTPRINT, pcbnew.PCB_TRACK, pcbnew.ZONE]]] = {}
+        # groups that are not part of the hierarchy, by footprint
+        exclude_groups: Set[GroupWrapper] = set()
 
         footprints = self._board.GetFootprints()  # type: List[pcbnew.FOOTPRINT]
         for footprint in footprints:
-            footprint_group = footprint.GetParentGroup()  # type: Optional[pcbnew.PCB_GROUP]
+            footprint_group = GroupWrapper(footprint.GetParentGroup())
             if not BoardUtils.footprint_path_startswith(footprint, self.path_prefix):
+                exclude_groups.add(footprint_group)
                 for pad in footprint.Pads():  # type: pcbnew.PAD
                     exclude_netcodes.add(pad.GetNetCode())
-                if footprint_group is not None:
-                    exclude_groups.add(GroupWrapper(footprint_group))
             else:
+                elts_by_group.setdefault(footprint_group, []).append(footprint)
                 for pad in footprint.Pads():
                     include_netcodes.add(pad.GetNetCode())
-                result.footprints.append(footprint)
-                if footprint_group is not None:
-                    result.groups.add(GroupWrapper(footprint_group))
-                else:
-                    has_ungrouped_footprints = True
 
+        include_netcodes = include_netcodes.difference(exclude_netcodes)
         for track in self._board.GetTracks():  # type: pcbnew.PCB_TRACK
-            include_netcodes = include_netcodes.difference(exclude_netcodes)
-            if track.GetNetCode() in include_netcodes or GroupWrapper(track.GetParentGroup()) in result.groups:
-                result.tracks.append(track)
+            if track.GetNetCode() in include_netcodes:
+                track_group = GroupWrapper(track.GetParentGroup())
+                elts_by_group.setdefault(track_group, []).append(track)
         for zone_id in range(self._board.GetAreaCount()):
             zone = self._board.GetArea(zone_id)  # type: pcbnew.ZONE
-            if zone.GetNetCode() in include_netcodes or GroupWrapper(zone.GetParentGroup()) in result.groups:
-                result.zones.append(zone)
+            if zone.GetNetCode() in include_netcodes:
+                track_group = GroupWrapper(zone.GetParentGroup())
+                elts_by_group.setdefault(track_group, []).append(zone)
 
-        # TODO warn on overlap include/exclude groups
-        # TODO copy groups to new board, ignoring top-level group
-        # algo: gather items by group, including ungrouped items
-        # for all groups, check for conflicts (groups containing other footprints, even recursively)
-        #   move items to ungrouped
-        # for remaining groups, remove sub-groups
-        # replicate groups, recursively, maintaining group structure
-        #   exception: if there is only one remaining group, it is the top-level board
+        # for exclude_groups in elts_by_group, move them to the None group
+        for group in exclude_groups:
+            if group in elts_by_group:
+                elts_by_group.setdefault(GroupWrapper(None), []).extend(elts_by_group[group])
+                # TODO warn on overlap include/exclude groups
+                del elts_by_group[group]
 
+        # prune groups with highest covering group
+        covering_groups = GroupWrapper.highest_covering_groups(list(elts_by_group.keys()))
+        for group in list(elts_by_group.keys()):
+            if group not in covering_groups:
+                del elts_by_group[group]
 
-        return result
+        ungrouped_elts = elts_by_group.pop(GroupWrapper(None), [])
+
+        return FilterResult(ungrouped_elts, set(elts_by_group.keys()))
