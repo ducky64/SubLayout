cdc6e7e86da6ced06ac986c8bcfa7eff7e926e7e
Richard Lin <elpato25@gmail.com>
2025-05-17 13:53:21 -0700

    tests r broken

--- sublayout/test_replicate.py
+++ sublayout/test_replicate.py
@@ -4,7 +4,7 @@ import unittest
 import pcbnew
 
 from .board_utils import BoardUtils
-from .replicate_sublayout import ReplicateSublayout
+from .replicate_sublayout import ReplicateSublayout, FootprintCorrespondence
 
 
 class ReplicateTestCase(unittest.TestCase):
@@ -13,24 +13,24 @@ class ReplicateTestCase(unittest.TestCase):
 
         sublayout_board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'McuSublayout.kicad_pcb'))  # type: pcbnew.BOARD
         anchor = board.FindFootprintByReference('U2')
-        sublayout = ReplicateSublayout(sublayout_board, board, anchor, BoardUtils.footprint_path(anchor)[:-1])
-        self.assertEqual(len(sublayout._correspondences), 8)
-        self.assertEqual(len(sublayout._extra_source_footprints), 0)
-        self.assertEqual(len(sublayout._extra_target_footprints), 0)
+        correspondence = FootprintCorrespondence.by_tstamp(sublayout_board, board, BoardUtils.footprint_path(anchor)[:-1])
+        self.assertEqual(len(correspondence.mapped_footprints), 8)
+        self.assertEqual(len(correspondence.source_only_footprints), 0)
+        self.assertEqual(len(correspondence.target_only_footprints), 0)
 
         sublayout_board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'UsbSublayout.kicad_pcb'))
         anchor = board.FindFootprintByReference('J1')
-        sublayout = ReplicateSublayout(sublayout_board, board, anchor, BoardUtils.footprint_path(anchor)[:-1])
-        self.assertEqual(len(sublayout._correspondences), 3)
-        self.assertEqual(len(sublayout._extra_source_footprints), 0)
-        self.assertEqual(len(sublayout._extra_target_footprints), 0)
+        correspondence = FootprintCorrespondence.by_tstamp(sublayout_board, board, BoardUtils.footprint_path(anchor)[:-1])
+        self.assertEqual(len(correspondence.mapped_footprints), 3)
+        self.assertEqual(len(correspondence.source_only_footprints), 0)
+        self.assertEqual(len(correspondence.target_only_footprints), 0)
 
         # test correspondences of a sub-hierarchy block in the sublayout
         anchor = board.FindFootprintByReference('R2')
-        sublayout = ReplicateSublayout(sublayout_board, board, anchor, BoardUtils.footprint_path(anchor)[:-1])
-        self.assertEqual(len(sublayout._correspondences), 2)
-        self.assertEqual(len(sublayout._extra_source_footprints), 1)  # just J1, which is out of scope
-        self.assertEqual(len(sublayout._extra_target_footprints), 0)
+        correspondence = FootprintCorrespondence.by_tstamp(sublayout_board, board, BoardUtils.footprint_path(anchor)[:-1])
+        self.assertEqual(len(correspondence.mapped_footprints), 2)
+        self.assertEqual(len(correspondence.source_only_footprints), 1)  # just J1, which is out of scope
+        self.assertEqual(len(correspondence.target_only_footprints), 0)
 
     def test_replicate(self):
         board = pcbnew.LoadBoard(os.path.join(os.path.dirname(__file__), 'tests', 'BareBlinkyComplete.kicad_pcb'))  # type: pcbnew.BOARD
